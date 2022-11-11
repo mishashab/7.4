e52da9e3c77597c16280df493c2467767b0f0736
Mikhail <misha.shabalin@mail.ru>
2022-11-11 19:40:16 +0300

    Ready

---
+++ 1.py
@@ -0,0 +1,45 @@
+class FlatIterator:
+
+    def __init__(self, list_of_list):
+        self.main_list = list_of_list
+        self.size_main_list = len(list_of_list)
+
+    def __iter__(self):
+        self.main_list_cursor = 0
+        self.nested_list_cursor = -1
+        return self
+
+    def __next__(self):
+        self.nested_list_cursor += 1
+
+        if self.nested_list_cursor >= len(
+                self.main_list[self.main_list_cursor]):
+            self.nested_list_cursor = 0
+        self.main_list_cursor += 1
+
+        if self.main_list_cursor >= len(self.main_list):
+            raise StopIteration
+
+        return self.main_list[self.main_list_cursor][self.nested_list_cursor]
+
+
+def test_1():
+
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+
+if __name__ == '__main__':
+    test_1()
